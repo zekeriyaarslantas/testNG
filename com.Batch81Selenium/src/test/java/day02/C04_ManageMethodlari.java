f850f0ef10a92d0feab2fb306569fbaae7ecd609
zekeriya <zekeriyaarslantas@gmail.com>
2022-10-09 00:49:13 +0300

    first commit

---
+++ com.Batch81Selenium/src/test/java/day02/C04_ManageMethodlari.java
@@ -0,0 +1,25 @@
+package day02;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C04_ManageMethodlari {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver","src/resources/drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+       // driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));//sayfa açılıncaya kadar max sn süresini test eder
+        driver.get("https://techproeducation.com");
+         /*
+        ileride wait daha geniş olarak ele alacağız.
+        1 sayfa acılırken ilk basta sayfanin icerisinde bulunan elementlere göre 1 yukleme süres,ne ihtiyac vardir.
+        veya 1 web elementin kullanılabilmesi icin zamana ihtiyac olabilir.
+        implicitywait bize sayfanin yüklenmesi ve sayfadaki elementlere ulasim icin beklenecek maximum süreyi belirleme
+        olanagi tanir
+         */
+        driver.close();
+    }
+}
